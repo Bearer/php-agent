c1db791dbb0ef6da0dee88b7ec615d907dcd669a
Paul Pasturel <paul.pasturel@cloud-temple.com>
2020-08-18 15:14:28 +0200

    PEST PHP integration

--- tests/AgentTest.php
+++ tests/AgentTest.php
@@ -7,40 +7,24 @@ use Bearer\Model\Configuration;
 use PHPUnit\Framework\TestCase;
 use Symfony\Component\OptionsResolver\Exception\UndefinedOptionsException;
 
-/**
- * Class AgentTest
- */
-class AgentTest extends TestCase
-{
-	/**
-	 * @return void
-	 */
-	public function testAgentInvalidArgument(): void
-	{
-		try {
-			Agent::init([
-				'wrong_parameter' => true
-			]);
-		} catch (\Exception $e) {
-			$this->assertInstanceOf(UndefinedOptionsException::class, $e);
-		}
-	}
+uses(TestCase::class)->group('agent');
 
-	/**
-	 * @return void
-	 */
-	public function testAgentConfiguration(): void
-	{
-		Agent::init([
-			'secretKey' => "secret_key",
-			'debug' => true
-		]);
+test('has invalid arguments', function () {
+	Agent::init([
+		'wrong_parameter' => true
+	]);
+})->throws(UndefinedOptionsException::class);
 
-		$configuration = Configuration::get();
+test('valid arguments', function () {
+	Agent::init([
+		'secretKey' => "secret_key",
+		'debug' => true
+	]);
 
-		$this->assertSame("secret_key", $configuration->getSecretKey());
-		$this->assertSame(false, $configuration->isDisabled());
-		$this->assertSame(true, $configuration->isDebug());
-		$this->assertSame(false, $configuration->isVerbose());
-	}
-}
+	$configuration = Configuration::get();
+
+	$this->assertSame("secret_key", $configuration->getSecretKey());
+	$this->assertSame(false, $configuration->isDisabled());
+	$this->assertSame(true, $configuration->isDebug());
+	$this->assertSame(false, $configuration->isVerbose());
+});
--- tests/Sanitizer/BodySanitizerTest.php
+++ tests/Sanitizer/BodySanitizerTest.php
@@ -5,52 +5,39 @@ namespace Bearer\Test\Sanitizer;
 use Bearer\Sanitizer\BodySanitizer;
 use PHPUnit\Framework\TestCase;
 
-/**
- * Class BodySanitizerTest
- * @package Bearer\Test\Sanitizer
- */
-class BodySanitizerTest extends TestCase
-{
-	private const sponge_bob = [
-		"name" => "Sponge Bob",
-		"age" => 12,
-		"friends" => ["patrick", "mr krab", "starman"]
-	];
+uses(TestCase::class)->group('body_sanitizer');
 
-	/**
-	 * @return void
-	 */
-	public function testSizeAndEmptyData(): void
-	{
-		$sanitizer = new BodySanitizer();
+$dataset = [
+	"name" => "Sponge Bob",
+	"age" => 12,
+	"friends" => ["patrick", "mr krab", "starman"]
+];
 
-		$this->assertSame("(no body)", $sanitizer(false, []));
-		$this->assertSame("(omitted due to size)", $sanitizer('test', [], 1048577));
-	}
+test('empty', function () {
+	$sanitizer = new BodySanitizer();
 
-	/**
-	 * @return void
-	 */
-	public function testJsonResult(): void
-	{
-		$this->assertSame(
-			json_encode(self::sponge_bob),
-			(new BodySanitizer())(json_encode(self::sponge_bob), [])
-		);
-	}
+	$this->assertSame("(no body)", $sanitizer(false, []));
+	$this->assertSame("(omitted due to size)", $sanitizer('test', [], 1048577));
+});
 
-	/**
-	 * @return void
-	 */
-	public function testSensitiveKey(): void
-	{
-		$this->assertSame(
-			json_encode(array_merge(self::sponge_bob, [
-				"secret" => "[FILTERED]"
-			]), JSON_NUMERIC_CHECK),
-			(new BodySanitizer())(json_encode(array_merge(self::sponge_bob, [
-				"secret" => "secret"
-			]), JSON_NUMERIC_CHECK), [])
-		);
-	}
-}
+test('json', function () use ($dataset) {
+	$sanitizer = new BodySanitizer();
+
+	$this->assertSame(
+		json_encode($dataset),
+		$sanitizer(json_encode($dataset), [])
+	);
+});
+
+test('sensitive_key', function () use ($dataset) {
+	$sanitizer = new BodySanitizer();
+
+	$this->assertSame(
+		json_encode(array_merge($dataset, [
+			"secret" => "[FILTERED]"
+		]), JSON_NUMERIC_CHECK),
+		$sanitizer(json_encode(array_merge($dataset, [
+			"secret" => "secret"
+		]), JSON_NUMERIC_CHECK), [])
+	);
+});
