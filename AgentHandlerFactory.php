bdd6b9f89920743a5a16e97cf17507fb1b0ae4ae
Paul Pasturel <paul.pasturel@cloud-temple.com>
2020-08-18 15:13:29 +0200

    Runkit removal and stream system integration

--- AgentHandlerFactory.php
+++ AgentHandlerFactory.php
@@ -12,6 +12,7 @@ use Bearer\Handler\Multi\MultiRemoveHandleHandler;
 use Bearer\Handler\Multi\MultiSetOptHandler;
 use Bearer\Handler\SetOptArrayHandler;
 use Bearer\Handler\SetOptHandler;
+use Bearer\Stream\HookProcessor;
 
 /**
  * Class AgentHandlerFactory
@@ -22,98 +23,25 @@ class AgentHandlerFactory
 	/**
 	 * @var array
 	 */
-	private const handlers = [
-		ExecHandler::class,
-		SetOptHandler::class,
-		SetOptArrayHandler::class,
-		MultiExecHandler::class,
-		MultiSetOptHandler::class,
-		MultiInfoReadHandler::class,
-		MultiAddHandleHandler::class,
-		MultiRemoveHandleHandler::class,
-		MultiCloseHandler::class
+	const handlers = [
+		'curl_exec' => ExecHandler::class,
+		'curl_setopt' => SetOptHandler::class,
+		'curl_setopt_array' => SetOptArrayHandler::class,
+		'curl_multi_exec' => MultiExecHandler::class,
+		'curl_multi_setopt' => MultiSetOptHandler::class,
+		'curl_multi_info_read' => MultiInfoReadHandler::class,
+		'curl_multi_add_handle' => MultiAddHandleHandler::class,
+		'curl_multi_remove_handle' => MultiRemoveHandleHandler::class,
+		'curl_multi_close' => MultiCloseHandler::class
 	];
 
 	/**
 	 * @throws \ReflectionException
 	 */
-	public static function build(): void
+	public static function build()
 	{
-		Agent::verbose('Runkit7', 'configuration start');
-		/** @var AbstractHandler $handler */
-		foreach (self::handlers as $handler) {
-			if (
-				self::exist($handler::getMethod()) &&
-				self::rename($handler::getMethod())
-			) {
-				self::add($handler, $handler::getMethod());
-				Agent::verbose('Runkit7', 'method overrided', $handler::getMethod());
-			}
-		}
-		Agent::verbose('Runkit7', 'configuration done');
+		Agent::verbose('Hook', 'configuration start');
+		(new HookProcessor(self::handlers))->intercept();
+		Agent::verbose('Hook', 'configuration done');
 	}
-
-	/**
-	 * @param string $method
-	 * @return bool
-	 */
-	private static function exist(string $method): bool
-	{
-		return function_exists($method);
-	}
-
-	/**
-	 * @param string $method
-	 * @return bool
-	 */
-	private static function rename(string $method): bool
-	{
-		return \runkit7_function_rename($method, "base_" . $method);
-	}
-
-	/**
-	 * @param string $class
-	 * @param string $method
-	 * @return bool
-	 * @throws \ReflectionException
-	 */
-	private static function add(string $class, string $method): bool
-	{
-		$reflection_class = new \ReflectionClass($class);
-		if (!$reflection_class->isSubclassOf(AbstractHandler::class)) {
-			throw new \RuntimeException(
-				sprintf('Class "%s" need to extend "%s"', $class, AbstractHandler::class)
-			);
-		}
-
-		$handler_method = $reflection_class->getMethod('__invoke');
-
-		return \runkit7_function_add($method,
-			self::getParametersString($handler_method, true),
-			"return (new $class())(" . self::getParametersString($handler_method) . ");"
-		);
-	}
-
-	/**
-	 * @param \ReflectionMethod $handler_method
-	 * @param bool $typed
-	 * @return string
-	 */
-	private static function getParametersString(\ReflectionMethod $handler_method, bool $typed = false): string
-	{
-		$parameters = array_map(function (\ReflectionParameter $parameter) use ($typed) {
-			return trim($typed ?
-				sprintf('%s$%s%s',
-					$parameter->isPassedByReference() ? '&' : '',
-					$parameter->getName(),
-					$parameter->isOptional() ? '=null' : ''
-				) :
-				sprintf('$%s', $parameter->getName())
-			);
-		}, $handler_method->getParameters());
-
-		return implode(', ', $parameters);
-	}
-
-
 }
